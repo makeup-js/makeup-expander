743532bc1ee32c9f8bbbea0e7f3c919722d409b3
Ian McBurnie <ianmcburnie@hotmail.com>
2019-04-24 17:42:03 -0700

    Upgraded devDeps

--- docs/static/bundle.js
+++ docs/static/bundle.js
@@ -591,7 +591,89 @@ https://github.com/joyent/node/blob/master/lib/module.js
     }
 })();
 
-$_mod.installed("makeup-expander$0.7.0", "custom-event-polyfill", "0.3.0");
+$_mod.installed("makeup-expander$0.7.0", "custom-event-polyfill", "1.0.7");
+$_mod.main("/custom-event-polyfill$1.0.7", "polyfill");
+$_mod.def("/custom-event-polyfill$1.0.7/polyfill", function(require, exports, module, __filename, __dirname) { // Polyfill for creating CustomEvents on IE9/10/11
+
+// code pulled from:
+// https://github.com/d4tocchini/customevent-polyfill
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
+
+(function() {
+  if (typeof window === 'undefined') {
+    return;
+  }
+
+  try {
+    var ce = new window.CustomEvent('test', { cancelable: true });
+    ce.preventDefault();
+    if (ce.defaultPrevented !== true) {
+      // IE has problems with .preventDefault() on custom events
+      // http://stackoverflow.com/questions/23349191
+      throw new Error('Could not prevent default');
+    }
+  } catch (e) {
+    var CustomEvent = function(event, params) {
+      var evt, origPrevent;
+      params = params || {};
+      params.bubbles = !!params.bubbles;
+      params.cancelable = !!params.cancelable;
+
+      evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent(
+        event,
+        params.bubbles,
+        params.cancelable,
+        params.detail
+      );
+      origPrevent = evt.preventDefault;
+      evt.preventDefault = function() {
+        origPrevent.call(this);
+        try {
+          Object.defineProperty(this, 'defaultPrevented', {
+            get: function() {
+              return true;
+            }
+          });
+        } catch (e) {
+          this.defaultPrevented = true;
+        }
+      };
+      return evt;
+    };
+
+    CustomEvent.prototype = window.Event.prototype;
+    window.CustomEvent = CustomEvent; // expose definition to window
+  }
+})();
+
+});
+$_mod.run("/custom-event-polyfill$1.0.7/polyfill");
+$_mod.installed("makeup-expander$0.7.0", "makeup-next-id", "0.0.2");
+$_mod.main("/makeup-next-id$0.0.2", "");
+$_mod.def("/makeup-next-id$0.0.2/index", function(require, exports, module, __filename, __dirname) { 'use strict';
+
+var sequenceMap = {};
+var defaultPrefix = 'nid';
+
+module.exports = function (el) {
+    var prefix = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : defaultPrefix;
+
+    // prevent empty string
+    var _prefix = prefix === '' ? defaultPrefix : prefix;
+
+    // initialise prefix in sequence map if necessary
+    sequenceMap[_prefix] = sequenceMap[_prefix] || 0;
+
+    if (!el.id) {
+        el.setAttribute('id', _prefix + '-' + sequenceMap[_prefix]++);
+    }
+};
+
+});
+$_mod.installed("makeup-expander$0.7.0", "makeup-exit-emitter", "0.0.4");
+$_mod.main("/makeup-exit-emitter$0.0.4", "");
+$_mod.installed("makeup-exit-emitter$0.0.4", "custom-event-polyfill", "0.3.0");
 $_mod.main("/custom-event-polyfill$0.3.0", "custom-event-polyfill");
 $_mod.def("/custom-event-polyfill$0.3.0/custom-event-polyfill", function(require, exports, module, __filename, __dirname) { // Polyfill for creating CustomEvents on IE9/10/11
 
@@ -640,31 +722,6 @@ try {
 
 });
 $_mod.run("/custom-event-polyfill$0.3.0/custom-event-polyfill");
-$_mod.installed("makeup-expander$0.7.0", "makeup-next-id", "0.0.2");
-$_mod.main("/makeup-next-id$0.0.2", "");
-$_mod.def("/makeup-next-id$0.0.2/index", function(require, exports, module, __filename, __dirname) { 'use strict';
-
-var sequenceMap = {};
-var defaultPrefix = 'nid';
-
-module.exports = function (el) {
-    var prefix = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : defaultPrefix;
-
-    // prevent empty string
-    var _prefix = prefix === '' ? defaultPrefix : prefix;
-
-    // initialise prefix in sequence map if necessary
-    sequenceMap[_prefix] = sequenceMap[_prefix] || 0;
-
-    if (!el.id) {
-        el.setAttribute('id', _prefix + '-' + sequenceMap[_prefix]++);
-    }
-};
-
-});
-$_mod.installed("makeup-expander$0.7.0", "makeup-exit-emitter", "0.0.4");
-$_mod.main("/makeup-exit-emitter$0.0.4", "");
-$_mod.installed("makeup-exit-emitter$0.0.4", "custom-event-polyfill", "0.3.0");
 $_mod.installed("makeup-exit-emitter$0.0.4", "makeup-next-id", "0.0.1");
 $_mod.main("/makeup-next-id$0.0.1", "");
 $_mod.def("/makeup-next-id$0.0.1/index", function(require, exports, module, __filename, __dirname) { 'use strict';
